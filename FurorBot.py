a08e66abeda5d6ca86d1b0d5af5d2062322a6782
Mike <mikesch1993@gmail.com>
2018-03-26 15:00:57 +0200

    New functions

--- FurorBot.py
+++ FurorBot.py
@@ -13,8 +13,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # if message.author == client.user and not message.content.startswith(':thinking:'):
-    #    return
+    if message.author == client.user and not message.content.startswith(':thinking:'):
+        return
 
     if message.content.startswith('$hello'):
         await client.send_message(message.channel, 'Hello! {0.author.mention}'.format(message))
@@ -26,8 +26,19 @@ async def on_message(message):
     if message.content.startswith('$purge'):
         await client.purge_from(message.channel)
 
+    if message.content.startswith('$nick'):
+        nickname = message.content.split(' ', maxsplit=1)
+        print('{} ; {}'.format(nickname[0], nickname[1]))
+        await client.change_nickname(message.author, nickname[1])
+
+
     if message.content.startswith(':thinking:'):
         await client.add_reaction(message, '\N{THINKING FACE}')
+
+@client.event
+async def on_member_join(member):
+    client.send_message(member, 'Welcome to my server, I hope you enjoy your stay! I\'m currently very much work in progress!')
+
 
 
 
