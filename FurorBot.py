646d94030beea178cab8ddd3f5b9572711c9e4e2
SCHMITZ Mike <mike.schmitz.002@student.uni.lu>
2018-03-26 10:07:45 +0200

    I'm just stupid, bot works

--- FurorBot.py
+++ FurorBot.py
@@ -5,20 +5,21 @@ TOKEN = 'NDI3NzIyNTEyMjY5MzEyMDAx.DZo1AA.sUlzLwKbCtMvG0x8Ddiv9xS04sU'
 client = discord.Client()
 
 @client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user)
+    print(client.user.id)
+    print('------')
+
+@client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+    if message.content.startswith('$hello'):
+        await client.send_message(message.channel, 'Hello! {0.author.mention}'.format(message))
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    if message.content.startswith('$thinking'):
+        await message.channel.send(':thinking:')
 
 client.run(TOKEN)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FurorDiscordBot
 Discord Bot for the Guild Furor on Tarren Mill EU
+
+# Interesting Links
+
+https://www.devdungeon.com/content/make-discord-bot-python
+
+http://discordpy.readthedocs.io/
